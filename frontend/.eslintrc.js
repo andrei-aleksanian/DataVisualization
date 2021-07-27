d5aa9070fbed2e70508b33444c1a0dd6750f0878
g28890aa <andrei.aleksanian@student.manchester.ac.uk>
2021-07-27 18:30:18 +0100

    Add automatic linters for frontend
    1. Eslint
    2. Prettier
    3. Stylelint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  ignorePatterns: ['node_modules', 'build', 'public', '.eslintrc.js'],
+  extends: ['plugin:react/recommended', 'airbnb-typescript', 'plugin:prettier/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 type HelloWorld = {
-  "Hello": string
-}
+  Hello: string;
+};
 
 function App() {
-  const [hi, setHi] = useState({"Hello": "doesn't work :("});
+  const [hi, setHi] = useState({ Hello: "doesn't work :(" });
   useEffect(() => {
     const getStuff = async () => {
-      const res = await fetch("http://localhost:8080/test");
+      const res = await fetch('http://localhost:8080/test');
       const data: HelloWorld = await res.json();
       setHi(data);
-    }
+    };
     getStuff();
-  }, [])
+  }, []);
 
   return (
     <div className="App">
       <div>If I am saying hello world, I am working:</div>
-      <div>Hello {hi["Hello"]}</div>
+      <div>
+        Hello
+        {hi.Hello}
+      </div>
     </div>
   );
 }
