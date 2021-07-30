51d5776a015fea08c3031a7ea8cec1ed54b84b3b
g28890aa <andrei.aleksanian@student.manchester.ac.uk>
2021-07-30 19:56:47 +0100

    Refactor data types
    "Data" types can now be reused accross the application

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -6,15 +6,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from ..angel.ANGEL import angel
 
-from ..types.data import DataOut, DataIn
-from data import EXAMPLE_DATA
+from ..types.data import DataOut
 
 app = FastAPI()
 # CORS from medium
 origins = [
     'http://localhost',
     'localhost',
-    # needed for development proxy (React runs at 3000)
+    # needed for development proxy (React runs at
+    # 3000)
     'http://localhost:3000',
     'localhost:3000'
 ]
@@ -38,6 +38,11 @@ app.add_middleware(
          summary="ANGEL Demo",
          response_model=DataOut)
 async def angel_demo():
-  # run the algorithm on sample data from jupyter
+    """
+    Demo endpoint with static output that runs the ANGEL algorithm.
+    Used for early development
+    """
+    # future note:
+    # check if result has 0s as the last column if user asked for 2d output
 
-  return angel()
+    return angel()
--- backend/app/types/data.py
+++ backend/app/types/data.py
@@ -1,24 +1,30 @@
-from pydantic import BaseModel, conlist
-from typing import List, Union, NewType
+"""
+Types.
 
-Data2D = NewType(
-    'Data2D', List[conlist(item_type=float, min_items=2, max_items=2)])
-Data3D = NewType(
-    'Data3D', List[conlist(item_type=float, min_items=3, max_items=3)])
+Different data types to be used accross the application
+"""
+from typing import List, NewType
+from pydantic import BaseModel, conlist
+
+# There is a flaw here - python can't specify a
+# list ending with 0 column
+Points = NewType('Points', List[conlist(
+    item_type=float, min_items=3, max_items=3)])
 
 
 class DataIn(BaseModel):
-  """
-  DataIn model that we will recieve from the browser
-  """
-  # M x N matrix of floats e.g. features of your ML data
-  data: List[List[float]]
-  dimension3D: bool = False
+    """
+    DataIn model that we will recieve from the browser
+    """
+    # M x N matrix of floats e.g. features of your
+    # ML data
+    data: List[List[float]]
+    dimension3D: bool = False
 
 
 class DataOut(BaseModel):
-  """
-  Data model to be used to process ANGEL and COVA on the server
-  """
-  points: Union[Data2D, Data3D]
-  labels: List[int]
+    """
+    Data model to be used to process ANGEL and COVA on the server
+    """
+    points: Points
+    labels: List[int]
