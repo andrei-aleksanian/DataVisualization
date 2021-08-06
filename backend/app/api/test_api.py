d12ee05e9f7cb92be3607222fdf0ba6a62ec923f
g28890aa <andrei.aleksanian@student.manchester.ac.uk>
2021-08-06 13:34:13 +0100

    Configure tests for backend
    1. CI
    2. Commit
    3. Test added for COVA demo endpoint (likely to be changed)

---
+++ backend/app/api/test_api.py
@@ -0,0 +1,11 @@
+# pylint: disable=C0114, C0116
+# docs will need to be updated for this module later
+from fastapi.testclient import TestClient
+from .api import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/cova-demo")
+    assert response.status_code == 200
