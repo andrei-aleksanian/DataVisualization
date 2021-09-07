3770eec28a23762f2216c4d8ed2ab3ee85969aa0
g28890aa <andrei.aleksanian@student.manchester.ac.uk>
2021-09-07 16:39:22 +0100

    Add colour scheme
    Now, if the labels number is less than 10, they will be predefined,
    not chosen from hsl wheel.

--- frontend/src/utils/getColors.ts
+++ frontend/src/utils/getColors.ts
@@ -1,34 +1,52 @@
+export const COLOR_SCHEME = [
+  '#fe5f55',
+  '#235789',
+  '#f1d302',
+  '#161925',
+  '#7e1f86',
+  '#E0CBA8',
+  '#f75c03',
+  '#ff8cc6',
+  '#419d78',
+  '#4c212a',
+];
+
 const getRandomColor = (colorNum: number, colorsTotal = 1): string => {
   return `hsl(${(colorNum * (360 / colorsTotal) + 200) % 360},100%,50%)`;
 };
 
-const findUniqueColors = (labels: number[]) => {
+const findUniqueColors = (uniqueLabels: number[]) => {
   const colors: string[] = [];
+
+  for (let i = 0; i < uniqueLabels.length; i += 1) {
+    const color = getRandomColor(i, uniqueLabels.length);
+    colors.push(color);
+  }
+
+  const colorMap = Object.assign({}, ...Array.from(uniqueLabels, (value, i) => ({ [value]: colors[i] })));
+
+  return colorMap;
+};
+
+const getUniqueLabels = (labels: number[]) => {
   const colorArr = new Set<number>();
 
   for (let i = 0; i < labels.length; i += 1) {
     colorArr.add(labels[i]);
   }
 
-  const reducedLabels = Array.from(colorArr);
-
-  for (let i = 0; i < reducedLabels.length; i += 1) {
-    const color = getRandomColor(i, reducedLabels.length);
-    colors.push(color);
-  }
-
-  const colorMap = Object.assign({}, ...Array.from(reducedLabels, (value, i) => ({ [value]: colors[i] })));
-
-  return colorMap;
+  return Array.from(colorArr);
 };
 
 export default (labels: number[]): string[] => {
   const colors: string[] = [];
-
-  const colorMap = findUniqueColors(labels);
+  const uniqueLabels = getUniqueLabels(labels);
+  let colorMap = null;
+  if (uniqueLabels.length > 10) colorMap = findUniqueColors(uniqueLabels);
 
   for (let i = 0; i < labels.length; i += 1) {
-    colors.push(colorMap[labels[i]]);
+    if (colorMap) colors.push(colorMap[labels[i]]);
+    else colors.push(COLOR_SCHEME[labels[i]]);
   }
 
   return colors;
