7b2b2a5fa756650923813a3617e5bb758d9ec2ce
g28890aa <andrei.aleksanian@student.manchester.ac.uk>
2021-09-22 12:21:20 +0100

    fix/hostlink

--- frontend/src/utils/hostLink.ts
+++ frontend/src/utils/hostLink.ts
@@ -1,1 +1,1 @@
-export default process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : `${window.location.hostname}/api`;
+export default process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : window.location.hostname;
