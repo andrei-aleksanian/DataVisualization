2145aaf372ba23e42daff3c43e5c1f5620cbbf3b
g28890aa <andrei.aleksanian@student.manchester.ac.uk>
2021-09-23 12:50:54 +0100

    Style home page
    1. Home page text updated
    2. Settigns should have position: relative.
       Loading modal bugs out otherwise.

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -4,14 +4,16 @@ import classes from './Home.module.scss';
 export const TEXT_LINK_LIBRARY = 'See an Example';
 export const TEXT_LINK_CUSTOM_DATA = 'Visualize Your Data';
 
-export const TEXT_H1 = 'COVA/ANGEL Visualization';
+export const TEXT_H1 = 'COVA / ANGEL';
+export const TEXT_H2 = 'Cohort Data Visualization';
 export const TEXT_P =
-  "Dimensionality reduction for cohort data visualization is to plot a high-dimensional dataset in 2D,3D spaces to help analysts identify data properties and gain insights visually. COVA and ANGEL are two novel data visualization algorithms that provide a simple but effective way to improve cohort data visualization performance comprehensively. This demo website illustrates examples embedded by these two algorithms under different hyper-parameter settings and supports users in visualizing their datasets. In addition, the fast-ANGEL version is applied to the users' custom datasets. The current restriction of input data format is '.mat'.";
+  'Dimensionality reduction for cohort data visualization is to plot a high-dimensional dataset in 2D,3D spaces to help analysts identify data properties and gain insights visually. COVA and ANGEL are two novel data visualization algorithms that provide a simple but effective way to improve cohort data visualization performance comprehensively. This demo website illustrates examples embedded by these two algorithms under different hyper-parameter settings and supports users in visualizing their datasets.';
 const Home = () => {
   return (
     <div className={classes.index}>
       <div className={classes.Container}>
         <h1>{TEXT_H1}</h1>
+        <h2>{TEXT_H2}</h2>
         <p>{TEXT_P}</p>
         <div className={classes.LinkWrapper}>
           <LinkHero text={TEXT_LINK_LIBRARY} link="/examples" />
